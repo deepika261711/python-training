8d0c0c6ed7c4da496723ff19f9ec681de5ca93e0
deepika261711 <deepikayerri343@gmail.com>
2025-12-26 14:53:21 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,3 +1,26 @@
-rows = 4
-for i in range(1, rows + 1):
-    print("*" * i)
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+def minion_game(string):
+    k=0
+    s=0
+    v="AEIOU"
+    for i in range(len(string)):
+        if string[i] in v:
+            k+=len(string)-i
+        else:
+            s+=len(string)-i
+    if k>s:
+                print("Kelvin",k)
+    elif s>k:
+                print("Stuart",s)
+    else:
+                print("Draw")
+string="BANANA"
+minion_game(string)
