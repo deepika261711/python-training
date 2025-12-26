682b1c891516f7c466585b4fbf204f8cd35a0e3b
deepika261711 <deepikayerri343@gmail.com>
2025-12-26 14:41:59 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,5 +1,22 @@
-x=int(input())
-for i in range(x):
-    for j in range(x):
-        print("*",end=" ")
-    print()
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+def leftover(a,b):
+    left=""
+    remove=set(b)
+    for ch in a:
+        if ch not in remove:
+            left+=ch
+    if left:
+       print(left)
+    else:
+       print("empty")
+A="ABCDEF"
+B="BDE"
+leftover(A,B)
