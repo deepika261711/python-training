8257396814bb65cdeedf8d8035c78db5076118c8
deepika261711 <deepikayerri343@gmail.com>
2025-12-26 15:46:07 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,5 +1,9 @@
-x=int(input())
-for i in range(x):
-    for j in range(i+1):
-        print(j+1,end=" ")
+n=int(input())
+for i in range(n):
+    for j in range(n):
+    
+        if j==0 or i==n-1 or i==j or i==0 or j==n-i-1 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
     print()
